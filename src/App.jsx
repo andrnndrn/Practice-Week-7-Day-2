ac10ef4f1d370abcef5f9dd762a07bf9e8e4f6cc
Andrean Nurdiana <andreannrdn@gmail.com>
2024-11-19 20:48:41 +0700

    add feature dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,68 @@ import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 import { useSelector, useDispatch } from "react-redux";
 import { changeLanguage } from "./redux/lang/action";
+import { toggleTheme } from "./redux/theme/action";
 
 const translation = {
   en: {
     title: "To-Do List",
     button: "English",
+    darkMode: "Dark Mode",
+    lightMode: "Light Mode",
   },
   id: {
     title: "Daftar Tugas",
     button: "Indonesia",
+    darkMode: "Mode Gelap",
+    lightMode: "Mode Terang",
   },
 };
 
 const App = () => {
   const lang = useSelector((state) => state.lang.lang);
+  const theme = useSelector((state) => state.theme.theme);
   const dispatch = useDispatch();
-const [todoToEdit, setTodoToEdit] = useState(null);
-  const { title, button } = translation[lang];
+  const [todoToEdit, setTodoToEdit] = useState(null);
+  const { title, button, darkMode, lightMode } = translation[lang];
 
   const toggleLanguage = () => {
     const newLang = lang === "en" ? "id" : "en";
     dispatch(changeLanguage(newLang));
   };
 
+  const handleToggleTheme = () => {
+    dispatch(toggleTheme());
+  };
+
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <div className="card shadow">
-            <div className="card-body">
-              <button onClick={toggleLanguage} className="btn btn-primary">
-                {button}
-              </button>
-              <h1 className="card-title text-center mb-4">{title}</h1>
-              <TodoInput todoToEdit={todoToEdit} setTodoToEdit={setTodoToEdit}/>
-              <TodoList onEdit={setTodoToEdit}/>
+    <div>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="card shadow">
+              <div className={`card-body ${theme}`}>
+                <div className="d-flex justify-content-between mb-3">
+                  <button onClick={toggleLanguage} className={`btn ${
+                      theme === "light" ? "btn-dark" : "btn-light"
+                    }`}>
+                    {button}
+                  </button>
+                  <button
+                    onClick={handleToggleTheme}
+                    className={`btn ${
+                      theme === "light" ? "btn-dark" : "btn-light"
+                    }`}
+                  >
+                    {theme === "light" ? darkMode : lightMode}
+                  </button>
+                </div>
+                <h1 className="card-title text-center mb-4">{title}</h1>
+                <TodoInput
+                  todoToEdit={todoToEdit}
+                  setTodoToEdit={setTodoToEdit}
+                />
+                <TodoList onEdit={setTodoToEdit} />
+              </div>
             </div>
           </div>
         </div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { legacy_createStore as createStore, combineReducers } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import todoReducer from "./todos/reducer";
 import langReducer from "./lang/reducer";
+import themeReducer from "./theme/reducer";
 
 const rootReducer = combineReducers({
     todo: todoReducer,
     lang: langReducer,
+    theme: themeReducer,
 });
 //store
 const store = createStore(rootReducer, composeWithDevTools());
